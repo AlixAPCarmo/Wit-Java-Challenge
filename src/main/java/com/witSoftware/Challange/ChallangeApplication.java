0d612742b7263797fbe486fcb1e12e56c38c7244
AlixAPCarmo <aluno23324@ipt.pt>
2025-01-08 23:19:56 +0000

    Project base created. Added modules rest and calculator. Rest module is now the entry point and not the parent module.

---
+++ src/main/java/com/witSoftware/Challange/ChallangeApplication.java
@@ -0,0 +1,11 @@
+package com.witSoftware.Challange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class ChallangeApplication {
+
+	public static void main(String[] args) {
+		System.out.println("ChallangeApplication start");
+	}
+}
